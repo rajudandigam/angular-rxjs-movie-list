cc73a09517f68e385cb9768f04a90f49014b1397
rajudandigam <dandigam.raju@gmail.com>
2019-11-17 22:17:36 -0800

    feat: added nav bar service to update the title

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavbarService } from './services/navbar.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  title$ = this.navbarservice.title;
+
+  constructor(private navbarservice: NavbarService) { }
 
   ngOnInit() {
   }
